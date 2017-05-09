4ed0c846a3a2799462ff209b4961745cd6834872
bobiscool <tomisugly@163.com>
2017-05-09 23:52:31 +0800

    --

--- nodeweb/requestHandler.js
+++ nodeweb/requestHandler.js
@@ -1,24 +1,35 @@
+var querystring = require("querystring");
+var fs = require("fs");
 var exec = require('child_process').exec;
-
+var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
 
 function start(response){
     //sleep(10000);
     console.log("request handler 'start' was called");
     var content = "empty";
 
-    exec("ls -lah",function(error,stdout,stderr){
-    content = stdout;
-    console.log(response);
     response.writeHead(200,{"Content-type":"text/plain"});
-    response.write(stdout);
+    response.write(body);
     response.end();
-});
 
 }
 
-function upload(){
+function upload(response,postData){
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write("you send postdata"+querystring.parse(postData).text);
+    response.end();
     console.log("request handler 'upload' was called");
-    return "hello upload";
 }
 
 function sleep(milliSeconds){
@@ -26,6 +37,22 @@ function sleep(milliSeconds){
     while (new Date().getTime()<startTime+milliSeconds);
 }
 
+function show(response,postData){
+    fs.readFile("/tmp/test.png","binary",function(error,file){
+        if(error){
+            response.writeHead("500",{"Content-Type":"text/plain"});
+            response.write(error+"\n");
+            response.end();
+        }else{
+            response.writeHead(200,{"Content-Type":"text/png"});
+            response.write(file,"binary");
+            response.end();
+        }
+    })
+}
+
+
 
 exports.start = start;
 exports.upload = upload;
+exports.show=show;
--- nodeweb/server.js
+++ nodeweb/server.js
@@ -1,12 +1,68 @@
 var http= require("http");
 var url = require("url");
+var util = require("util")
+var formidable = require("formidable");
 var route = require("./router.js");
 
 console.log(route);
 function start(route,handle){
 function onRequest(request,response){
     //console.log(response);
+
+//    if(req.url=="/upload"&&req.method.toLowerCase()=="post"){
+//        var form = new formidable.IncomingForm();
+//        form.parse(req,function(){
+//        res.writeHead(200,{"content-type":"text/plain"});
+//            res.write("file recieved");
+//            res.end(util.inspect({fields:fields,files:files}));
+//
+//        });
+//        return;
+//    }
+
+if (request.url == '/upload' && request.method.toLowerCase() == 'post') {
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(err, fields, files) {
+      request.writeHead(200, {'content-type': 'text/plain'});
+      request.write('received upload:\n\n');
+      request.end(util.inspect({fields: fields, files: files}));
+    });
+
+    request.writeHead(200, {'content-type': 'text/html'});
+  request.end(
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+
+    return;
+  }
+
+  // show a file upload form
+  
+
+
+
+
     var pathname = url.parse(request.url).pathname;
+    request.setEncoding('utf8');
+
+    request.addListener("data",function(postDataChunk){
+    postData +=postDatatChunk;
+    console.log("recieved dataChunk")+postDataChunk+".";
+
+    });
+
+    request.addListener("end",function(){
+        route(handle,pathname,response,request);
+    });
+
+
+
     route(handle,pathname,response);
 }
 
